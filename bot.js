16bcd4a80a835900b7c6b04c7eb8ba67c5e807f0
BobTBuilder <34952067+BobTBuilder@users.noreply.github.com>
2017-12-30 13:25:26 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,5 +29,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'AIDAN') {
+    	message.reply('**IS A DUMBASS**');
+    }
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
