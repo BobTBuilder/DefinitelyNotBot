ff707442d66c036f1358e9f5f1f1f29f33b9efa7
BobTBuilder <34952067+BobTBuilder@users.noreply.github.com>
2017-12-29 20:37:52 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,10 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'rape') {
+    	message.channel.send('NIGGERS!');
+  	}
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
